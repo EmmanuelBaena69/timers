ad3813dae0fdf0f0de16ad1680db57d0cc186aef
SakuSaku <travispastrana075@gmail.com>
2023-03-20 13:06:49 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace timers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tmr1 = new System.Windows.Forms.Timer(components);
+            tmr2 = new System.Windows.Forms.Timer(components);
+            tmr3 = new System.Windows.Forms.Timer(components);
+            lbl1 = new Label();
+            lbl2 = new Label();
+            lbl3 = new Label();
+            btn1 = new Button();
+            SuspendLayout();
+            // 
+            // tmr1
+            // 
+            tmr1.Tick += tmr1_Tick;
+            // 
+            // tmr2
+            // 
+            tmr2.Tick += tmr2_Tick;
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(55, 132);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(50, 20);
+            lbl1.TabIndex = 0;
+            lbl1.Text = "label1";
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Location = new Point(376, 132);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(50, 20);
+            lbl2.TabIndex = 1;
+            lbl2.Text = "label2";
+            // 
+            // lbl3
+            // 
+            lbl3.AutoSize = true;
+            lbl3.Location = new Point(672, 132);
+            lbl3.Name = "lbl3";
+            lbl3.Size = new Size(50, 20);
+            lbl3.TabIndex = 2;
+            lbl3.Text = "label3";
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(352, 341);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(94, 29);
+            btn1.TabIndex = 3;
+            btn1.Text = "Start";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn1);
+            Controls.Add(lbl3);
+            Controls.Add(lbl2);
+            Controls.Add(lbl1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmr1;
+        private System.Windows.Forms.Timer tmr2;
+        private System.Windows.Forms.Timer tmr3;
+        private Label lbl1;
+        private Label lbl2;
+        private Label lbl3;
+        private Button btn1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+using System.Drawing;
+
+namespace timers
+{
+    public partial class Form1 : Form
+    {
+        int cont1 = 0, cont2 = 0, cont3 = 0;
+        bool flag100 = false;
+        bool flag1 = true;
+        bool flag20 = false;
+        bool flag2_1 = true;
+        bool flag50 = false;
+        bool flag3_1 = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            tmr1.Enabled = true;
+            tmr2.Enabled = true;
+            tmr3.Enabled = true;
+        }
+
+        private void tmr1_Tick(object sender, EventArgs e)
+        {
+            if (cont1 == 100)
+            {
+                flag100 = true;
+                flag1 = false;
+            }
+
+            if (cont1 == 1)
+            {
+                flag100 = false;
+                flag1 = true;
+            }
+            if (flag1 == true && flag100 == false)
+            {
+                cont1 = cont1 + 1;
+                lbl1.Text = cont1.ToString();
+            }
+            if (flag100 == true && flag1 == false)
+            {
+                cont1 = cont1 - 1;
+                lbl1.Text = cont1.ToString();
+            }
+        }
+
+        private void tmr2_Tick(object sender, EventArgs e)
+        {
+            if (cont2 == 20)
+            {
+                flag20 = true;
+                flag2_1 = false;
+            }
+
+            if (cont2 == 1)
+            {
+                flag20 = false;
+                flag2_1 = true;
+            }
+            if (flag2_1 == true && flag20 == false)
+            {
+                cont2 = cont2 + 1;
+                lbl2.Text = cont2.ToString();
+            }
+            if ((flag20 == true && flag2_1 == false) && (flag1 == true && cont1 <= 20))
+            {
+                cont2 = cont2 - 1;
+                lbl2.Text = cont2.ToString();
+            }
+        }
+    }
+}
